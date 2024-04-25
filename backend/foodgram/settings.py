8199d2b5babbb8cba901378b6c292f36716708cc
unknown <abakharev74@gmail.com>
2024-04-26 02:20:11 +0500

    Foodgram 0.6

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
-        }
     }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
